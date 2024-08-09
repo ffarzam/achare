54601208b950f70b9a1dd85beed573a38962b274
farzam <farzam@arianatech.ir>
2024-08-09 19:18:34 +0330

    CHORE: update password permissions

--- account/permissions/password_permissions.py
+++ account/permissions/password_permissions.py
@@ -5,6 +5,4 @@ class IsPasswordSet(BasePermission):
     message = "Permission denied, you have already set your password."
 
     def has_object_permission(self, request, view, obj):
-        if request.data.get("password") or request.data.get("password").strip() in [""]:
-            return not bool(obj.password)
-        return True
+        return not bool(obj.password)
